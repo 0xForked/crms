6b170a2f18327a53c04523102a0ac779dac5f436
A. A. Sumitro <hello@aasumitro.id>
2020-07-04 18:43:15 +0800

    add quil tolbar image, align, sub, super and image link handler function

--- public/assets/js/custom_quill_editor.js
+++ public/assets/js/custom_quill_editor.js
@@ -1,15 +1,29 @@
 var editor = new Quill('#quill-container', {
     modules: {
-        toolbar: [
-            [{header: [1, 2, false]}],
-            ['bold', 'italic', 'underline', 'strike', 'code', 'link'],
-            [{list: 'ordered'}, {list: 'bullet'}, 'blockquote', 'code-block']
-        ]
+        toolbar: {
+            container: [
+                [{header: [1, 2, 3, 4, 5, 6, false]}],
+                ['bold', 'italic', 'underline', 'strike', { 'script': 'sub'}, { 'script': 'super' }, 'code', 'link'],
+                [{ 'align': [] }],
+                [{list: 'ordered'}, {list: 'bullet'}, 'blockquote', 'code-block', 'image'],
+            ],
+            handlers: {
+                image: imageHandler
+            }
+        }
     },
     scrollingContainer: '#scrolling-container',
     placeholder: 'Compose an epic...',
     theme: 'bubble'
 });
+
+function imageHandler() {
+    var range = editor.getSelection();
+    var value = prompt('What is the image URL');
+    if(value){
+        editor.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+    }
+}
 
 var form = document.getElementById('post-form');
 form.onsubmit = function() {
